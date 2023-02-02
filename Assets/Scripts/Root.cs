319064903064101dab0660aa287a11840b55b7cb
TheBlipBloop <blipbloopdev@gmail.com>
2023-02-01 17:56:52 -0700

    Smooth root movement

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -1,5 +1,3 @@
-// #define JankyCollisionDetection
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,22 +6,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Root : MonoBehaviour
 {
-
-#if JankyCollisionDetection
-	[System.Serializable]
-	protected struct RootCollisionNode
-	{
-		public Vector2 position;
-		public Vector2 direction;
-
-		public RootCollisionNode(Vector2 position, Vector2 direction)
-		{
-			this.position = position;
-			this.direction = direction;
-		}
-	}
-#endif
-
 	[SerializeField]
 	protected Rigidbody2D body;
 
@@ -38,10 +20,6 @@ public class Root : MonoBehaviour
 
 	protected LinkedList<Vector2> rootPositions = new LinkedList<Vector2>();
 
-#if JankyCollisionDetection
-	protected List<RootCollisionNode> collisionPoints = new List<RootCollisionNode>();
-#endif
-
 	protected Vector2 mousePosition;
 
 	private Vector2 lastRecordedRootPosition;
@@ -50,9 +28,6 @@ public class Root : MonoBehaviour
 	void Start()
 	{
 		rootPositions.AddFirst(transform.position);
-#if JankyCollisionDetection
-		collisionPoints.Add(new RootCollisionNode(transform.position, transform.right));
-#endif
 	}
 
 	// Update is called once per frame
@@ -60,32 +35,18 @@ public class Root : MonoBehaviour
 	{
 		UpdateMousePosition();
 		UpdateMovement();
+
+		if (rootRenderer.positionCount > 0)
+		{
+			rootRenderer.SetPosition(rootRenderer.positionCount - 1, transform.position);
+		}
 	}
 
 	protected void UpdateMovement()
 	{
-#if JankyCollisionDetection
-		RootCollisionNode debugg = collisionPoints[collisionPoints.Count - 1];
-
-		Debug.DrawRay(debugg.position, debugg.direction, Color.green);
-		Debug.DrawRay(debugg.position, transform.position - (Vector3)debugg.position, Color.red);
-		Debug.Log(Vector2.Angle(((Vector2)transform.position - debugg.position).normalized, debugg.direction));
-#endif
 		if (Input.GetKey(KeyCode.Mouse0))
 		{
 			Grow(GetDirectionToMouse());
-
-#if JankyCollisionDetection
-			RootCollisionNode lastCollisionNode = collisionPoints[collisionPoints.Count - 1];
-
-			Debug.DrawRay(lastCollisionNode.position, lastCollisionNode.direction, Color.green);
-			Debug.DrawRay(lastCollisionNode.position, (Vector3)lastCollisionNode.position - transform.position, Color.green);
-
-			if (Vector2.Distance(transform.position, lastCollisionNode.position) > 3f || Vector2.Angle(((Vector2)transform.position - lastCollisionNode.position).normalized, lastCollisionNode.direction) > 6f)
-			{
-				collisionPoints.Add(new RootCollisionNode(transform.position, GetDirectionToMouse()));
-			}
-#endif
 			return;
 		}
 		if (Input.GetKey(KeyCode.Mouse1))
@@ -93,14 +54,8 @@ public class Root : MonoBehaviour
 			UnGrow();
 			return;
 		}
-		body.velocity = Vector2.Lerp(body.velocity, Vector2.zero, Time.fixedDeltaTime * 8f);
 
-		LinkedListNode<Vector2> node = rootPositions.First;
-		while (node.Next.Next != null)
-		{
-			Debug.DrawLine(node.Value, node.Next.Value, Color.yellow);
-			node = node.Next;
-		}
+		body.velocity = Vector2.Lerp(body.velocity, Vector2.zero, Time.fixedDeltaTime * 8f);
 	}
 
 
@@ -113,7 +68,6 @@ public class Root : MonoBehaviour
 			rootPositions.AddFirst(transform.position);
 
 			rootRenderer.positionCount++;
-			rootRenderer.SetPosition(rootRenderer.positionCount - 1, transform.position);
 
 			lastRecordedRootPosition = transform.position;
 		}
@@ -130,7 +84,7 @@ public class Root : MonoBehaviour
 
 		body.velocity = (mostRecentPosition - transform.position).normalized * baseMoveSpeed;
 
-		if (Vector3.Distance(transform.position, mostRecentPosition) < rootRecordPositionInterval / 2f)
+		if (rootRenderer.positionCount > 0 && Vector3.Distance(transform.position, mostRecentPosition) < rootRecordPositionInterval / 2f)
 		{
 			rootRenderer.positionCount--;
 			rootPositions.RemoveFirst();
--- Assets/Scripts/RootRenderer.cs
+++ Assets/Scripts/RootRenderer.cs
@@ -17,13 +17,6 @@ public class RootRenderer : MonoBehaviour
 	[SerializeField]
 	protected float thicknessPulseSpeed = 5f;
 
-	[SerializeField]
-	protected GameObject collisionPrefab;
-
-	Vector3 lastAddPosition = Vector3.negativeInfinity;
-
-	Vector3 lastColliderSpawnPosition = Vector3.negativeInfinity;
-
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -34,19 +27,5 @@ public class RootRenderer : MonoBehaviour
 	void Update()
 	{
 		rootRenderer.widthMultiplier = thickness + Mathf.Sin(Time.time / thicknessPulseSpeed * Mathf.PI) * thicknessPulseMagnitude;
-		if (Vector2.Distance(transform.position, lastAddPosition) > 0.15f)
-		{
-			rootRenderer.positionCount++;
-			rootRenderer.SetPosition(rootRenderer.positionCount - 1, transform.position);
-
-			lastAddPosition = transform.position;
-		}
-
-		if (Vector2.Distance(transform.position, lastColliderSpawnPosition) > collisionPrefab.GetComponent<CircleCollider2D>().radius * 2)
-		{
-			Instantiate(collisionPrefab, transform.position, Quaternion.identity);
-
-			lastColliderSpawnPosition = transform.position;
-		}
 	}
 }
