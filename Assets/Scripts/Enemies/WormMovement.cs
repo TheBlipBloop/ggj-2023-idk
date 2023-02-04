56a7a5059f4d8e2eb6590aa279cb1bca7de212d8
Zero497 <61260935+Zero497@users.noreply.github.com>
2023-02-04 12:19:39 -0700

    Update WormMovement.cs

--- Assets/Scripts/Enemies/WormMovement.cs
+++ Assets/Scripts/Enemies/WormMovement.cs
@@ -14,12 +14,14 @@ public class WormMovement : MonoBehaviour
         curTargIndex = 1;
         Transform currentTarget = points[1];
         currentDirection = Vector3.Normalize(currentTarget.position-this.transform.position);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(currentDirection * speed * Time.deltaTime);
+        this.transform.Translate(currentDirection * speed * Time.deltaTime, Space.World);
+        this.transform.Translate(new Vector3(0,0, -this.transform.position.z), Space.World);
         if(Mathf.Abs(this.transform.position.x-points[curTargIndex].position.x) < 0.25)
         {
             if(Mathf.Abs(this.transform.position.x - points[curTargIndex].position.x) < 0.25)
@@ -31,6 +33,14 @@ public class WormMovement : MonoBehaviour
                 }
                 Transform currentTarget = points[curTargIndex];
                 currentDirection = Vector3.Normalize(currentTarget.position - this.transform.position);
+                Vector3 worldX = new Vector3(1, 0, 0);
+                float theta = Mathf.Acos(Vector3.Dot(currentDirection, worldX)) * Mathf.Rad2Deg;
+                if(Vector3.Cross(currentDirection, worldX).z > 0)
+                {
+                    theta = -theta;
+                }
+                this.transform.Rotate(new Vector3(-this.transform.rotation.x, -this.transform.rotation.y, -this.transform.rotation.z));
+                this.transform.Rotate(new Vector3(0, 0, theta));
             }
         }
     }
