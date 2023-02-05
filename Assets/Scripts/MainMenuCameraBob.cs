920dd7c3f2dfa606b5f07263a0bb4f78f7dd601a
TheBlipBloop <blipbloopdev@gmail.com>
2023-02-04 22:20:32 -0700

    Finish main menu polish pass

--- Assets/Scripts/MainMenuCameraBob.cs
+++ Assets/Scripts/MainMenuCameraBob.cs
@@ -10,6 +10,9 @@ public class MainMenuCameraBob : MonoBehaviour
 	[SerializeField]
 	protected float bobFrequency = 10f;
 
+	[SerializeField]
+	protected Vector3 targetPosition = new Vector3(0, 0, -10);
+
 	protected Vector3 startPosition;
 
 	// Start is called before the first frame update
@@ -21,6 +24,8 @@ public class MainMenuCameraBob : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		transform.position = startPosition + Vector3.up * Mathf.Sin(Time.time / bobFrequency * Mathf.PI) * bobMagnitude;
+		targetPosition = startPosition + Vector3.up * Mathf.Sin(Time.time / bobFrequency * Mathf.PI) * bobMagnitude;
+		targetPosition.z = -10f;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 3f);
 	}
 }
